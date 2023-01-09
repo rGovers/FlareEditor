8a51bdd2c7831b37e1b72b6d5bf00a77b7c117f9
rGovers <govers.river.r@gmail.com>
2023-01-09 21:14:07 +1100

    Advanced profiler view

--- FlareEditor/include/Windows/ProfilerWindow.h
+++ FlareEditor/include/Windows/ProfilerWindow.h
@@ -2,9 +2,24 @@
 
 #include "Windows/Window.h"
 
+#include <string>
+#include <vector>
+
+struct ProfileSnapshot;
+
 class ProfilerWindow : public Window
 {
 private:
+    struct ProfileFrameSelection
+    {
+        std::string SnapshotName;
+        std::string FrameName;
+    };
+
+    std::vector<ProfileFrameSelection> m_selections;
+
+    uint32_t GetFrameIndex(const ProfileSnapshot& a_snapshot) const;
+    void SetFrameIndex(const std::string_view& a_snapshotName, const std::string_view& a_frameName);
 
 protected:
 
